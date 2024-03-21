becbf205ec792128ee66e6dd4f705f0498bc928a
Mischa Barthel <barthel.m@hsleiden.nl>
2024-03-21 17:03:50 +0100

    Aanpassingen

--- orderline_dw/my_prefect/init_and_full_load.py
+++ orderline_dw/my_prefect/init_and_full_load.py
@@ -4,11 +4,11 @@ from orderline_dw.my_prefect import load
 from orderline_dw.my_prefect import init
 
 @flow
-def all_flow():
+def init_and_full_load_flow():
     init.init_flow()
     load.full_load_flow()
 
 
 if __name__ == "__main__":
-    all_flow()
+    init_and_full_load_flow()
 
